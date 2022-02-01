3b03fefdc449755f5f037604ce90f5cc38229a2f
Boris <boris021@mail.ru>
2022-02-01 17:16:46 +0500

    fix

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,4 +1,3 @@
-
 /*
 Документация по работе в шаблоне: https://www.lightgalleryjs.com/docs/
 Документация плагина: https://www.lightgalleryjs.com/docs/
@@ -6,9 +5,14 @@
 */
 
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile, FLS } from "./functions.js";
+import {
+	isMobile,
+	FLS
+} from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import {
+	flsModules
+} from "./modules.js";
 
 // Подключение базового набора функционала
 import lightGallery from 'lightgallery';
@@ -16,7 +20,7 @@ import lightGallery from 'lightgallery';
 // Плагины
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
 // import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
-//import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
+import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
 
 // Базовые стили
 import '@scss/libs/gallery/lightgallery.scss';
@@ -24,7 +28,7 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lg-thumbnail.scss';
 // import '@scss/libs/gallery/lg-video.scss';
 // import '@scss/libs/gallery/lg-autoplay.scss';
-// import '@scss/libs/gallery/lg-zoom.scss';
+import '@scss/libs/gallery/lg-zoom.scss';
 // import '@scss/libs/gallery/lg-pager.scss';
 // import '@scss/libs/gallery/lg-fullscreen.scss';
 // import '@scss/libs/gallery/lg-share.scss';
@@ -34,7 +38,7 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lg-relative-caption.scss';
 
 // Все стили
-// import '@scss/libs/gallery/lightgallery-bundle.scss';
+import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
 const galleries = document.querySelectorAll('[data-gallery]');
@@ -45,6 +49,8 @@ if (galleries.length) {
 			gallery,
 			galleryClass: lightGallery(gallery, {
 				// plugins: [lgZoom, lgThumbnail],
+				plugins: [lgZoom],
+				zoom: true,
 				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 				speed: 500,
 			})
@@ -53,8 +59,3 @@ if (galleries.length) {
 	// Добавляем в объект модулей
 	flsModules.gallery = galleyItems;
 }
-
-
-
-
-
