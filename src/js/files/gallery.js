491bf8c14eacd2db64de53ab1aee2d87504ceb80
Boris <boris021@mail.ru>
2022-02-02 10:04:44 +0500

    fix

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -19,13 +19,13 @@ import lightGallery from 'lightgallery';
 
 // Плагины
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
-// import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
+import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
 import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
 
 // Базовые стили
 import '@scss/libs/gallery/lightgallery.scss';
 // Стили дополнений
-// import '@scss/libs/gallery/lg-thumbnail.scss';
+import '@scss/libs/gallery/lg-thumbnail.scss';
 // import '@scss/libs/gallery/lg-video.scss';
 // import '@scss/libs/gallery/lg-autoplay.scss';
 import '@scss/libs/gallery/lg-zoom.scss';
@@ -38,7 +38,7 @@ import '@scss/libs/gallery/lg-zoom.scss';
 // import '@scss/libs/gallery/lg-relative-caption.scss';
 
 // Все стили
-import '@scss/libs/gallery/lightgallery-bundle.scss';
+// import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
 const galleries = document.querySelectorAll('[data-gallery]');
@@ -49,7 +49,7 @@ if (galleries.length) {
 			gallery,
 			galleryClass: lightGallery(gallery, {
 				// plugins: [lgZoom, lgThumbnail],
-				plugins: [lgZoom],
+				plugins: [lgZoom, lgThumbnail],
 				zoom: true,
 				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 				speed: 500,
